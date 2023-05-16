d401301c43bf6c07990d5df618365cfbf952ea10
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-05-16 17:08:07 +0200

    Add class Genre and use composition object

--- Models/Movie.php
+++ Models/Movie.php
@@ -4,19 +4,28 @@ class Movie
     public $name;
     public $genres;
 
-    public function __construct($name, $genres)
+    public function __construct($name, array $genres)
     {
+        foreach ($genres as $genre) {
+            if (!($genre instanceof Genre)) {
+                trigger_error('Array of objects passed to constructor must contain instances of Genre', E_USER_ERROR);
+            }
+        }
         $this->name = $name;
         $this->genres = $genres;
     }
 
-    function get_name()
+    public function get_name()
     {
         return $this->name;
     }
 
-    function get_genres_to_string()
+    public function get_genres_to_string()
     {
-        return implode(", ", $this->genres);
+        $return_string = "";
+        foreach ($this->genres as $genre) {
+            $return_string = $return_string . $genre->get_name() . ", ";
+        }
+        return rtrim($return_string, ", ");
     }
 }
