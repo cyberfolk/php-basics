f00f4a1a1351874369482b44a87065294ceb165d
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-05-16 14:18:09 +0200

    Add model base

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include __DIR__ . "/assets/partials/head.php" ?>
+
+<body class="bg-primary">
+
+    <?php include __DIR__ . "/assets/partials/header.php" ?>
+    <main id="app_main">
+        <div class="container">
+            ciaone
+        </div>
+        <!-- /.container -->
+    </main>
+    <!-- /#app_main -->
+
+</body>
+
+</html>
