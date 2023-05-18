5e01f6d73e3c47a8d4ffb3d96122f70f613b95cc
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-05-18 14:07:48 +0200

    Add Mouse class

---
+++ Models/Component_subclass/Mouse.php
@@ -0,0 +1,19 @@
+
+<?php
+class Mouse extends Component
+{
+    public function __construct(protected string $brand, protected string $model, protected float $price, protected int $dpi)
+    {
+        parent::__construct($brand, $model, $price);
+        $this->dpi = $dpi;
+    }
+
+    public function get_dpi()
+    {
+        return $this->dpi;
+    }
+    public function to_string()
+    {
+        return "<strong>" . get_class() . "</strong>"  . ": " . $this->brand . ", " . $this->model . ", " . $this->price . "$, " . $this->dpi . "DPI";
+    }
+}
