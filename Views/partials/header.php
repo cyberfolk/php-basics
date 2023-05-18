9026e156a972bcd80e85988e3b011a0aee7d8b42
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-05-18 14:29:07 +0200

    Add fotter and add product.php as partials

--- Views/partials/header.php
+++ Views/partials/header.php
@@ -1,4 +1,4 @@
-<header id="app_header" class="text-center pt-5 pb-4 text-light">
+<header id="app_header" class="text-center pt-5 pb-4">
     <div class="container">
         <h1 class="text-center text-warning"> Movie DB </h1>
     </div>
